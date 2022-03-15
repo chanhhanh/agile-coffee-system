fad58248b3286767bc3453e016300d80ff736ac3
Ngan217 <81847898+Ngan217@users.noreply.github.com>
2022-03-15 21:24:23 +0700

    AA-37 Login-Signup

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -1,1 +1,26 @@
-login
+<div class="container">
+  <div class="row mt-5">
+    <div class="col-sm-6 p-5" style="color: #636363">
+      <h3>Login</h3>
+      <hr>
+      
+      <form class="" action="<?php echo base_url('/account/login'); ?>" method="post">
+        <div class="row">
+          <div class="form-group">
+            <label for="email">Email : </label>
+            <input type="text" class="form-control" name="email" id="email" placeholder="Enter your email..." value="">
+          </div>
+          <div class="form-group">
+            <label for="password">Password : </label>
+            <input type="password" class="form-control" name="password" id="password" value="" placeholder="Enter password...">
+          </div>
+          <?php if (isset($validation)) { ?>
+            <div class="alert m-auto text-danger" role="alert">
+              <?php echo $validation->listErrors(); ?>
+            </div>
+          <?php } ?>
+		  </div>
+		  <button type="submit" class="btn btn-danger col-12 mt-2">Signin</button>
+      </form>
+</div>
+
