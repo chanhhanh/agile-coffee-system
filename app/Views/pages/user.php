62ec09e18cd36dbb21e03ee128959c6b9ec93b87
Captain17 <thoailt.cnthongtin@gmail,com>
2022-03-17 17:59:59 +0700

    AA-21-view-update-profile

--- app/Views/pages/user.php
+++ app/Views/pages/user.php
@@ -1,1 +1,24 @@
-user
+<div class="container">
+  <div class="row mt-5">
+    <div class="col-sm-6 p-5" style="color: #636363">
+      <h3>Profile</h3>
+      <hr>
+      <i class="bi bi-pencil-square"></i>
+      <form class="" action="" method="post">
+        <div class="row">
+          <div class="form-group">
+            <label for="email">Email : </label>
+            <label for="email"> <p><?php echo session()->get('email'); ?></p></label>
+          </div>
+          <div class="form-group">
+            <label for="email">Username : </label>
+            <label for="email"> <p><?php echo session()->get('username'); ?></p></label>
+            <br></br>
+            <label for="email">
+                <a class="nav-link px-2" href="<?php echo base_url('/update'); ?>">Edit</a>
+                <a class="nav-link px-2" href="<?php echo base_url('logout'); ?> ">Logout</a>
+               
+            </label>
+        </div>
+      </form>
+</div>
