466ee98824aad6de8e939cdd3c99e9fbfcb0f5c0
ttphuongvi <trthphuongvi2901@gmail.com>
2022-03-28 13:41:23 +0700

    push branch A55
    ....

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -10,7 +10,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed&display=swap" rel="stylesheet">
   <link href=<?php echo base_url('/css/styles.css') ?> rel="stylesheet">
-
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
   <title>Harold's Coffee</title>
 </head>
 
@@ -18,31 +18,80 @@
   <div class="shadow-sm rounded sticky-top">
     <nav class="navbar navbar-light bg-light justify-content-between">
       <div></div>
-      <ul class="navbar-nav flex-row mx-5">
+      <ul class="navbar-nav flex-row mx-5 swap3">
         <?php
         $uri = service('uri');
         ?>
         <?php if (session()->get('isLoggedIn')) : ?>
           </li>
-          <li class="nav-item <?= ($uri->getSegment(1) == 'user' ? 'active' : null) ?>">
+          <li class=" nav-item <?= ($uri->getSegment(1) == 'user' ? 'active' : null) ?>">
             <a class="nav-link px-2" href="<?php echo base_url('user'); ?>"><?php echo session()->get('username'); ?></a>
           </li>
           <li class="nav-item">
             <a class="nav-link px-2" href="<?php echo base_url("logout") ?>">Logout</a>
           </li>
         <?php else : ?>
+          <li>
+            <div class="row ">
+              <div class="col-lg-12 col-sm-12 col-12 main-section ">
+                <div class="dropdown swapCart">
+                  <button type=" button" class="btn btn-light" data-toggle="dropdown" onclick="on()">
+                    <i class="bi bi-cart"></i> <span class="badge badge-pill badge-danger" style="color: black">(0)</span>
+                  </button>
+                  <!-- <button type="button" class="btn btn-secondary" data-container="body" data-toggle="popover" data-placement="left" data-content="Vivamus
+sagittis lacus vel augue laoreet rutrum faucibus.">
+                  Popover on bottom
+                </button> -->
+                  <div class=" dropdown-menu" id="popup" onclick="off()">
+                    <div class="row total-header-section">
+                      <div class="col-lg-6 col-sm-6 col-6">
+                        <!-- <i class="fa fa-shopping-cart" aria-hidden="true"></i> <span class="badge badge-pill badge-danger">3</span> -->
+                      </div>
+                      <div class="col-lg-6 col-sm-6 col-6 total-section text-right">
+                        <p>Total: <span class="text-info">$2,000</span></p>
+                      </div>
+                    </div>
+                    <div class="row cart-detail">
+                      <div class="col-lg-4 col-sm-4 col-4 cart-detail-img">
+                        <!--  -->
+                      </div>
+                      <div class="col-lg-8 col-sm-8 col-8 cart-detail-product">
+                        <p>Iced White Mocha</p>
+                        <span class="price text-info"> $250.22</span> <span class="count"> Quantity:01</span>
+                      </div>
+                    </div>
+
+                    <div class="row">
+                      <div class="col-lg-12 col-sm-12 col-12 text-center checkout">
+                        <button class="btn btn-primary btn-block">Checkout</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
           <li class="nav-item <?= ($uri->getSegment(1) == 'login' ? 'active' : null) ?>">
             <a class="nav-link px-2" href="<?php echo base_url('login'); ?>">Login</a>
           </li>
           <li class="nav-item <?= ($uri->getSegment(1) == 'register' ? 'active' : null) ?>">
             <a class="nav-link px-2" href="<?php echo base_url('register'); ?>">Signup</a>
           </li>
+          <script>
+            function on() {
+              document.getElementById("popup").style.display = "block";
+            }
+
+            function off() {
+              document.getElementById("popup").style.display = "none";
+            }
+          </script>
         <?php endif; ?>
         <!-- <li class="nav-item">
                     cart</li> -->
       </ul>
     </nav>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light px-5 frontof">
       <a class="navbar-brand" style="
     font-family: 'Sansita Swashed', cursive;" href="<?php echo base_url(); ?>">Harold's Coffee</a>
       <div class="collapse navbar-collapse" id="navbarNav">
