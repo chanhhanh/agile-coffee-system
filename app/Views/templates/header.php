9beafbf35483528cfb57338611b4a3e22f69fb00
HuynhManDat <JuniorKevin110@gmail.com>
2022-03-28 23:19:47 +0700

    Update shopping cart

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -5,7 +5,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed&display=swap" rel="stylesheet">
@@ -16,76 +17,70 @@
 
 <body>
   <div class="shadow-sm rounded sticky-top">
-    <nav class="navbar navbar-light bg-light justify-content-between">
-      <div></div>
+    <nav class="navbar navbar-light bg-light justify-content-end">
       <ul class="navbar-nav flex-row mx-5 swap3">
         <?php
         $uri = service('uri');
         ?>
         <?php if (session()->get('isLoggedIn')) : ?>
-          </li>
-          <li class=" nav-item <?= ($uri->getSegment(1) == 'user' ? 'active' : null) ?>">
-            <a class="nav-link px-2" href="<?php echo base_url('user'); ?>"><?php echo session()->get('username'); ?></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link px-2" href="<?php echo base_url("logout") ?>">Logout</a>
-          </li>
+        <li class=" nav-item <?= ($uri->getSegment(1) == 'user' ? 'active' : null) ?>">
+          <a class="nav-link px-2" href="<?php echo base_url('user'); ?>"><?php echo session()->get('username'); ?></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link px-2" href="<?php echo base_url("logout") ?>">Logout</a>
+        </li>
         <?php else : ?>
-          <li>
-            <div class="row ">
-              <div class="col-lg-12 col-sm-12 col-12 main-section ">
-                <div class="dropdown swapCart">
-                  <button type=" button" class="btn btn-light" data-toggle="dropdown" onclick="on()">
-                    <i class="bi bi-cart"></i> <span class="badge badge-pill badge-danger" style="color: black">(0)</span>
-                  </button>
-                  <!-- <button type="button" class="btn btn-secondary" data-container="body" data-toggle="popover" data-placement="left" data-content="Vivamus
-sagittis lacus vel augue laoreet rutrum faucibus.">
-                  Popover on bottom
-                </button> -->
-                  <div class=" dropdown-menu" id="popup" onclick="off()">
-                    <div class="row total-header-section">
-                      <div class="col-lg-6 col-sm-6 col-6">
-                        <!-- <i class="fa fa-shopping-cart" aria-hidden="true"></i> <span class="badge badge-pill badge-danger">3</span> -->
-                      </div>
-                      <div class="col-lg-6 col-sm-6 col-6 total-section text-right">
-                        <p>Total: <span class="text-info">$2,000</span></p>
-                      </div>
-                    </div>
-                    <div class="row cart-detail">
-                      <div class="col-lg-4 col-sm-4 col-4 cart-detail-img">
-                        <!--  -->
-                      </div>
-                      <div class="col-lg-8 col-sm-8 col-8 cart-detail-product">
-                        <p>Iced White Mocha</p>
-                        <span class="price text-info"> $250.22</span> <span class="count"> Quantity:01</span>
-                      </div>
-                    </div>
-
-                    <div class="row">
-                      <div class="col-lg-12 col-sm-12 col-12 text-center checkout">
-                        <button class="btn btn-primary btn-block">Checkout</button>
-                      </div>
-                    </div>
-                  </div>
+        <li class="nav-item px-2 swapCart">
+        <div class="">
+            <button type=" button" class="btn btn-light dropdown-toggle" onclick="toggle()">
+              <i class="bi bi-cart"></i> <span class="badge badge-pill badge-danger" style="color: black">(0)</span>
+            </button>
+            <div class="dropdown-menu p-4" id="popup">
+              <div class="row cart-detail">
+                <div class="col-lg-12 col-sm-12 col-12 cart-detail-product">
+                  <p>Iced White Mocha</p>
+                  <span class="price text-info"> $250.22</span> <span class="count"> Quantity:01</span>
+                </div>
+              </div>
+              <div class="row total-header-section">
+                <div class="col-lg-6 col-sm-6 col-6 total-section text-right">
+                  <p>Total: <span class="text-info">$2,000</span></p>
+                </div>
+              </div>
+              <div class="row mt-4">
+                <div class="col-lg-12 col-sm-12 col-12 text-center checkout">
+                  <button class="btn btn-primary btn-block">Checkout</button>
                 </div>
               </div>
             </div>
-          </li>
-          <li class="nav-item <?= ($uri->getSegment(1) == 'login' ? 'active' : null) ?>">
-            <a class="nav-link px-2" href="<?php echo base_url('login'); ?>">Login</a>
-          </li>
-          <li class="nav-item <?= ($uri->getSegment(1) == 'register' ? 'active' : null) ?>">
-            <a class="nav-link px-2" href="<?php echo base_url('register'); ?>">Signup</a>
-          </li>
-          <script>
-            function on() {
-              document.getElementById("popup").style.display = "block";
+        </li>
+        <li class="nav-item <?= ($uri->getSegment(1) == 'login' ? 'active' : null) ?>">
+          <a class="nav-link px-2" href="<?php echo base_url('login'); ?>">Login</a>
+        </li>
+        <li class="nav-item <?= ($uri->getSegment(1) == 'register' ? 'active' : null) ?>">
+          <a class="nav-link px-2" href="<?php echo base_url('register'); ?>">Signup</a>
+        </li>
+        <script>
+            let isOpen = false;
+            let item = document.querySelector(".dropdown-menu");
+            function toggle(){
+             
+              isOpen=!isOpen;
+              item.style.display = isOpen ?"block": "none" ;
+              document.addEventListener("keydown",function(e){
+                if(e.key==="Escape"){
+                  isOpen=false;
+                  item.style.display = isOpen ? "block":"none";
+                }
+              })
+              document.addEventListener("click",function(e){
+                if( !item.contains(e.target) && !document.querySelector(".dropdown-toggle").contains(e.target)){
+                  isOpen=false
+                  item.style.display = isOpen ? "block":"none";
+                }
+              })
             }
-
-            function off() {
-              document.getElementById("popup").style.display = "none";
-            }
-          </script>
+        </script>
         <?php endif; ?>
         <!-- <li class="nav-item">
                     cart</li> -->
