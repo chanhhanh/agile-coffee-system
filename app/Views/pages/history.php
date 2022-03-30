bb1a6386cc9dd22992b0137514315ad6e0fe0b70
ttphuongvi <trthphuongvi2901@gmail.com>
2022-03-30 20:49:37 +0700

    fix css footer and create history order

---
+++ app/Views/pages/history.php
@@ -0,0 +1,39 @@
+<!-- <div class="form-history">
+    
+    <div class="form-title-info-history row-history">
+        <div>Name</div>
+        <div>Size</div>
+        <div>Price</div>
+    </div>
+    <div class="form-content-history row-history">
+        <div>Iced White Mocha</div>
+        <div>M</div>
+        <div>5.50</div>
+    </div>
+
+</div> -->
+
+<div class="form-history">
+    
+    <table class="table-history">
+        <tr class="form-title-info-history col-history">
+            <th>Name</th>
+            <th>Size</th>
+            <th>Amount</th>
+            <th>Price</th>
+        </tr>
+        <tr class="form-content-history" >
+            <td>Iced White Mocha</td>
+            <td>M</td>
+            <td>1</td>
+            <td>5.50</td>
+        </tr>
+    </div>
+    </table>
+    <div>
+        <div class="">Totals</div>
+    </div>
+    <div>
+        <div class="total-history">Repurchase</div>
+    </div>
+</div>
