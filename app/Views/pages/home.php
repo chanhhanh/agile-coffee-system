11d74777f4ec9445ae7d2f78655cbff0eab39514
Nguyễn Ngọc Phương Quỳnh <nnpquynh01vvk@gmail.com>
2022-03-30 22:37:10 +0700

    User should be informed with the latest promotions and new/seasonal products

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -2,11 +2,6 @@
 
 <div class="main">
     <!-- carousel -->
-    <div class="cart-ss1-right">
-    <div class="change-quantity decrease">-</div>
-    <div class="amount">1</div>
-    <div class="change-quantity increase">+</div>
-    </div>
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
