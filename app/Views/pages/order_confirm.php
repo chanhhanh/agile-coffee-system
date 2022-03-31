9802ff91a117e07184fb5ba4072e9de026f4c394
Tứ Linh <tranthitulinh1305@gmail.com>
2022-03-31 09:00:18 +0700

    Validation Email Phone (Order Confirm)
    Còn lỗi btn remove

--- app/Views/pages/order_confirm.php
+++ app/Views/pages/order_confirm.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="./public/bootstrap/bootstrap.min.css">
     <!-- Font awesome -->
     <link rel="stylesheet" href="./public/font-awesome/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <!-- Custom css-->
     <link rel="stylesheet" href="./public/css/order-detail.css" type="text/css">
 </head>
@@ -41,16 +42,17 @@
                                     <input type="number" value="1" min="1" style="width:40px">
                                 </div>
                                 <div class="product-removal">
-                                    <button class="remove-product"
+                                    <!-- <button class="remove-product"
                                         style="border: 0; padding: 4px 8px; background-color: #c66; color: #fff; font-family: font-bold; font-size: 12px; border-radius: 3px;">
                                         Remove
-                                    </button>
-
+                                    </button> -->
+                                    <i class="bi bi-trash3 remove-product"
+                                        style="border: 0; padding: 4px 8px; background-color: #c66; color: #fff; font-family: font-bold; font-size: 12px; border-radius: 3px;"></i>
                                     <span class="text-muted" style="padding-left:23px">23600000</span>
                             </li>
                             <li class="list-group-item d-flex justify-content-between lh-condensed">
                                 <div>
-                                    <h6 class="my-0">Pink Drink</h6>
+                                    <h6 class="my-0">Iced White Mochac</h6>
                                     <small class="text-muted product-line-price">11800000.00 x 2</small>
                                 </div>
 
@@ -58,11 +60,12 @@
                                     <input type="number" value="1" min="1" style="width:40px">
                                 </div>
                                 <div class="product-removal">
-                                    <button class="remove-product"
+                                    <!-- <button class="remove-product"
                                         style="border: 0; padding: 4px 8px; background-color: #c66; color: #fff; font-family: font-bold; font-size: 12px; border-radius: 3px;">
                                         Remove
-                                    </button>
-
+                                    </button> -->
+                                    <i class="bi bi-trash3 remove-product"
+                                        style="border: 0; padding: 4px 8px; background-color: #c66; color: #fff; font-family: font-bold; font-size: 12px; border-radius: 3px;"></i>
                                     <span class="text-muted" style="padding-left:23px">23600000</span>
                             </li>
                             <!-- <li class="list-group-item d-flex justify-content-between lh-condensed">
@@ -112,41 +115,42 @@
                             </div>
                             <div class="col-md-12">
                                 <label for="exampleInputPhone">Phone</label>
-                                <input type="text" class="form-control" id="exampleInputPhone"
-                                    aria-describedby="phoneHelp" placeholder="Your phone">
+                                <input id="exampleInputPhone" name="areaNo" class="form-control" type="tel" required
+                                    aria-describedby="phoneHelp" placeholder="Your phone" pattern="[0-9]{3}"
+                                    title="Please Enter Correct Phone Format" aria-label="Area code">
                             </div>
                             <div class="col-md-12">
                                 <label for="exampleInputEmail">Email</label>
-                                <input type="text" class="form-control" id="exampleInputEmail"
-                                    aria-describedby="emailHelp" placeholder="Your email">
+                                <!-- <input type="text" class="form-control" id="exampleInputEmail"
+                                    aria-describedby="emailHelp" placeholder="Your email"> -->
+                                <input class="form-control" id="exampleInputEmail" type="email" size="64" maxLength="64"
+                                    required placeholder="username@gmail.com" pattern=".+@gmail\.com"
+                                    title="Please Enter Correct Email Format">
+                            </div>
+                            <h4 class="mb-3">Payments</h4>
+                            <!--  Tiền mặt MoMo ZaloPay ShopeePay-->
+                            <div class="d-block my-3">
+                                <div class="custom-control custom-radio">
+                                    <input id="httt-1" name="httt_ma" type="radio" class="custom-control-input"
+                                        required="" value="1">
+                                    <label class="custom-control-label" for="httt-1">Cash on Delivery</label>
+                                </div>
+                                <div class="custom-control custom-radio">
+                                    <input id="httt-2" name="httt_ma" type="radio" class="custom-control-input"
+                                        required="" value="2">
+                                    <label class="custom-control-label" for="httt-2">Direct Bank Transfer</label>
+                                </div>
+                                <div class="custom-control custom-radio">
+                                    <input id="httt-3" name="httt_ma" type="radio" class="custom-control-input"
+                                        required="" value="3">
+                                    <label class="custom-control-label" for="httt-3">Paypal</label>
+                                    <img src="https://www.logolynx.com/images/logolynx/c3/c36093ca9fb6c250f74d319550acac4d.jpeg"
+                                        alt="" width="50">
+                                </div>
                             </div>
 
                         </div>
-
-                        <h4 class="mb-3">Payments</h4>
-                        <!--  Tieền mặt MoMo ZaloPay ShopeePay-->
-                        <div class="d-block my-3">
-                            <div class="custom-control custom-radio">
-                                <input id="httt-1" name="httt_ma" type="radio" class="custom-control-input" required=""
-                                    value="1">
-                                <label class="custom-control-label" for="httt-1">Cash on Delivery</label>
-                            </div>
-                            <div class="custom-control custom-radio">
-                                <input id="httt-2" name="httt_ma" type="radio" class="custom-control-input" required=""
-                                    value="2">
-                                <label class="custom-control-label" for="httt-2">Direct Bank Transfer</label>
-                            </div>
-                            <div class="custom-control custom-radio">
-                                <input id="httt-3" name="httt_ma" type="radio" class="custom-control-input" required=""
-                                    value="3">
-                                <label class="custom-control-label" for="httt-3">Paypal</label>
-                                <img src="https://www.logolynx.com/images/logolynx/c3/c36093ca9fb6c250f74d319550acac4d.jpeg"
-                                    alt="" width="50">
-                            </div>
-                        </div>
-
                     </div>
-                </div>
             </form>
 
         </div>
