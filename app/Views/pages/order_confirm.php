91c0dcda526c6e97982bf2fc2ffe00186feda581
HuynhManDat <JuniorKevin110@gmail.com>
2022-03-31 22:59:24 +0700

    Add data from database to frontend

--- app/Views/pages/order_confirm.php
+++ app/Views/pages/order_confirm.php
@@ -32,42 +32,27 @@
                             <span class="badge badge-secondary badge-pill">2</span>
                         </h4>
                         <ul class="list-group mb-3">
-                            <li class="product list-group-item d-flex justify-content-between lh-condensed">
-                                <div>
-                                    <h6 class="my-0">Iced White Mochac</h6>
-                                    <small class="text-muted product-line-price">11800000.00 x 2</small>
-                                </div>
+                            <?php $sum=0 ?>
+                            <?php foreach($cart_item as $item) : ?>
+                                <li class="product list-group-item d-flex justify-content-between lh-condensed">
+                                    <div>
+                                        <h6 class="my-0"><?php echo $item["coffee_name"] ?> (<?php echo $item["size"] ?>)</h6>
+                                        <small class="text-muted product-line-price">Customized: <?php echo $item["preferences"] ?></small>
+                                    </div>
 
-                                <div class="product-quantity" style="width: 52px">
-                                    <input type="number" value="1" min="1" style="width:40px">
-                                </div>
-                                <div class="product-removal">
-                                    <!-- <button class="remove-product"
-                                        style="border: 0; padding: 4px 8px; background-color: #c66; color: #fff; font-family: font-bold; font-size: 12px; border-radius: 3px;">
-                                        Remove
-                                    </button> -->
-                                    <i class="bi bi-trash3 remove-product"
-                                        style="border: 0; padding: 4px 8px; background-color: #c66; color: #fff; font-family: font-bold; font-size: 12px; border-radius: 3px;"></i>
-                                    <span class="text-muted" style="padding-left:23px">23600000</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between lh-condensed">
-                                <div>
-                                    <h6 class="my-0">Iced White Mochac</h6>
-                                    <small class="text-muted product-line-price">11800000.00 x 2</small>
-                                </div>
-
-                                <div class="product-quantity" style="width: 52px">
-                                    <input type="number" value="1" min="1" style="width:40px">
-                                </div>
-                                <div class="product-removal">
-                                    <!-- <button class="remove-product"
-                                        style="border: 0; padding: 4px 8px; background-color: #c66; color: #fff; font-family: font-bold; font-size: 12px; border-radius: 3px;">
-                                        Remove
-                                    </button> -->
-                                    <i class="bi bi-trash3 remove-product"
-                                        style="border: 0; padding: 4px 8px; background-color: #c66; color: #fff; font-family: font-bold; font-size: 12px; border-radius: 3px;"></i>
-                                    <span class="text-muted" style="padding-left:23px">23600000</span>
-                            </li>
+                                    <div class="product-quantity" style="width: 52px">
+                                        <input type="number" value="<?php echo $item["quantity"] ?>" min="1" style="width:40px">
+                                    </div>
+                                    <div class="product-removal">
+                                        <!-- <button class="remove-product"
+                                            style="border: 0; padding: 4px 8px; background-color: #c66; color: #fff; font-family: font-bold; font-size: 12px; border-radius: 3px;">
+                                            Remove
+                                        </button> -->
+                                        <i class="bi bi-trash3 remove-product" style="border: 0; padding: 4px 8px; background-color: #c66; color: #fff; font-family: font-bold; font-size: 12px; border-radius: 3px;"></i>
+                                        <span class="text-muted" style="padding-left:23px"><?php echo $item["total_amount"] ?></span>
+                                </li>
+                                <?php $sum+=$item["total_amount"] ?>
+                            <?php endforeach; ?>
                             <!-- <li class="list-group-item d-flex justify-content-between lh-condensed">
                                 <div>
                                     <h6 class="my-0">Pink Drink</h6>
@@ -77,7 +62,7 @@
                             </li> -->
                             <li class="list-group-item d-flex justify-content-between">
                                 <span>Total money</span>
-                                <strong>143520000</strong>
+                                <strong>$<?=$sum?></strong>
                             </li>
                         </ul>
                         <div class="input-group">
@@ -87,56 +72,44 @@
                             </div>
                         </div>
                         <hr class="mb-4">
-                        <button class="btn btn-primary btn-lg btn-block" type="submit" name="btnDatHang"
-                            style="margin-top: 0; margin-bottom: 0; margin-left: 353px; margin-right: 0;">Order</button>
+                        <button class="btn btn-primary btn-lg btn-block" type="submit" name="btnDatHang" style="margin-top: 0; margin-bottom: 0; margin-left: 353px; margin-right: 0;">Order</button>
                     </div>
                     <div class="col-md-8 order-md-1" style="width: 600px; margin-left:30px">
                         <h4 class="mb-3">Customer information</h4>
-
                         <div class="row">
                             <div class="col-md-12">
-                                <label for="exampleInputName">Full Name</label>
-                                <input type="text" class="form-control" id="exampleInputName"
-                                    aria-describedby="nameHelp" placeholder="Full your name">
-                            </div>
-                            <div class="col-md-12">
-                                <label for="exampleInputAddress">Address</label>
-                                <input type="text" class="form-control" id="exampleInputAddress"
-                                    aria-describedby="addressHelp" placeholder="Your address">
-                            </div>
-                            <div class="col-md-12">
-                                <label for="exampleInputPhone">Phone</label>
-                                <input id="exampleInputPhone" name="areaNo" class="form-control" type="tel" required
-                                    aria-describedby="phoneHelp" placeholder="Your phone" pattern="[0-9]{3}"
-                                    title="Please Enter Correct Phone Format" aria-label="Area code">
-                            </div>
-                            <div class="col-md-12">
-                                <label for="exampleInputEmail">Email</label>
-                                <!-- <input type="text" class="form-control" id="exampleInputEmail"
+                                    <label for="exampleInputName">Full Name</label>
+                                    <input type="text" class="form-control" id="exampleInputName" aria-describedby="nameHelp" placeholder="Your full name" value="<?php echo session()->get('fullname'); ?>">
+                                </div>
+                                <div class="col-md-12">
+                                    <label for="exampleInputAddress">Address</label>
+                                    <input type="text" class="form-control" id="exampleInputAddress" aria-describedby="addressHelp" placeholder="Your address">
+                                </div>
+                                <div class="col-md-12">
+                                    <label for="exampleInputPhone">Phone</label>
+                                    <input id="exampleInputPhone" name="areaNo" class="form-control" type="tel" required aria-describedby="phoneHelp" placeholder="Your phone" pattern="[0-9]{10}" title="Please Enter Correct Phone Format" aria-label="Area code" value="<?php echo session()->get('phone_number'); ?>">
+                                </div>
+                                <div class="col-md-12">
+                                    <label for="exampleInputEmail">Email</label>
+                                    <!-- <input type="text" class="form-control" id="exampleInputEmail"
                                     aria-describedby="emailHelp" placeholder="Your email"> -->
-                                <input class="form-control" id="exampleInputEmail" type="email" size="64" maxLength="64"
-                                    required placeholder="username@gmail.com" pattern=".+@gmail\.com"
-                                    title="Please Enter Correct Email Format">
-                            </div>
+                                    <input class="form-control" id="exampleInputEmail" type="email" size="64" maxLength="64" required placeholder="username@gmail.com" pattern=".+@gmail\.com" title="Please Enter Correct Email Format" value="<?php echo session()->get('email'); ?>">
+                                </div>
                             <h4 class="mb-3">Payments</h4>
                             <!--  Tiền mặt MoMo ZaloPay ShopeePay-->
                             <div class="d-block my-3">
                                 <div class="custom-control custom-radio">
-                                    <input id="httt-1" name="httt_ma" type="radio" class="custom-control-input"
-                                        required="" value="1">
+                                    <input id="httt-1" name="httt_ma" type="radio" class="custom-control-input" required="" value="1">
                                     <label class="custom-control-label" for="httt-1">Cash on Delivery</label>
                                 </div>
                                 <div class="custom-control custom-radio">
-                                    <input id="httt-2" name="httt_ma" type="radio" class="custom-control-input"
-                                        required="" value="2">
+                                    <input id="httt-2" name="httt_ma" type="radio" class="custom-control-input" required="" value="2">
                                     <label class="custom-control-label" for="httt-2">Direct Bank Transfer</label>
                                 </div>
                                 <div class="custom-control custom-radio">
-                                    <input id="httt-3" name="httt_ma" type="radio" class="custom-control-input"
-                                        required="" value="3">
+                                    <input id="httt-3" name="httt_ma" type="radio" class="custom-control-input" required="" value="3">
                                     <label class="custom-control-label" for="httt-3">Paypal</label>
-                                    <img src="https://www.logolynx.com/images/logolynx/c3/c36093ca9fb6c250f74d319550acac4d.jpeg"
-                                        alt="" width="50">
+                                    <img src="https://www.logolynx.com/images/logolynx/c3/c36093ca9fb6c250f74d319550acac4d.jpeg" alt="" width="50">
                                 </div>
                             </div>
 
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -2,123 +2,145 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed&display=swap" rel="stylesheet">
-    <link href=<?php echo base_url('/css/styles.css') ?> rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <title>Harold's Coffee</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed&display=swap" rel="stylesheet">
+  <link href=<?php echo base_url('/css/styles.css') ?> rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+  <title>Harold's Coffee</title>
 </head>
 
 <body>
-    <div class="shadow-sm rounded sticky-top">
-        <nav class="navbar navbar-light bg-light justify-content-end">
-            <ul class="navbar-nav flex-row mx-5 swap3">
-                <?php
+  <div class="shadow-sm rounded sticky-top">
+    <nav class="navbar navbar-light bg-light justify-content-end">
+      <ul class="navbar-nav flex-row mx-5 swap3">
+        <?php
         $uri = service('uri');
         ?>
-                <?php if (session()->get('isLoggedIn')) : ?>
-                <li class="nav-item px-2 swapCart">
-                    <div class="">
-                        <button type=" button" class="btn btn-light dropdown-toggle" onclick="toggle()">
-                            <i class="bi bi-cart"></i> <span class="badge badge-pill badge-danger"
-                                style="color: black">(0)</span>
-                        </button>
-                        <div class="dropdown-menu p-4" id="popup">
-                            <div class="row cart-detail">
-                                <div class="col-lg-12 col-sm-12 col-12 cart-detail-product">
-                                    <p>Iced White Mocha</p>
-                                    <span class="price text-info"> $250.22</span> <span class="count">
-                                        Quantity:01</span>
-                                </div>
-                            </div>
-                            <div class="row total-header-section">
-                                <div class="col-lg-6 col-sm-6 col-6 total-section text-right">
-                                    <p>Total: <span class="text-info">$2,000</span></p>
-                                </div>
-                            </div>
-                            <div class="row mt-4">
-                                <div class="col-lg-12 col-sm-12 col-12 text-center checkout">
-                                    <!-- <a class="nav-link text-dark" href="<?php echo base_url("/menu") ?>">Menu</a> -->
-                                    <button class="btn btn-primary btn-block">
-                                        <a style="text-decoration: none; color: white"
-                                            href="<?php echo base_url("/order_confirm") ?>">Checkout
-                                        </a>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                </li>
-                <li class=" nav-item <?= ($uri->getSegment(1) == 'user' ? 'active' : null) ?>">
-                    <a class="nav-link px-2"
-                        href="<?php echo base_url('user'); ?>"><?php echo session()->get('username'); ?></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link px-2" href="<?php echo base_url("logout") ?>">Logout</a>
-                </li>
-                <script>
-                let isOpen = false;
-                let item = document.querySelector(".dropdown-menu");
+        <?php if (session()->get('isLoggedIn')) : ?>
+          <li class="nav-item px-2 swapCart">
+            <div class="">
+              <button type=" button" class="btn btn-light dropdown-toggle" onclick="toggle()">
+                <i class="bi bi-cart"></i> <span class="badge badge-pill badge-danger" style="color: black">(0)</span>
+              </button>
+              <div class="dropdown-menu p-4" id="popup">
+                <div class="row cart-detail">
+                    <table class="table table-borderless">
+                    <tbody>
+                      <?php $sum=0 ?>
+                    <?php foreach ($cart_item as $item) : ?>
+                      <tr>
+                        <td><?php echo $item["coffee_name"] ?></td>
+                        <td><?php echo $item["size"] ?></td>
+                        <td><?php echo $item["quantity"] ?></td>
+                      </tr>
+                      <tr>
+                        <?php if($item["preferences"] != null){ ?>
+                          <td> Customized: <?php echo $item["preferences"] ?></td>
+                        <?php } ?>
+                      </tr>
+                      <?php $sum+=$item["total_amount"] ?>
+                    <?php endforeach; ?>
+                    </tbody>
+                  </table>
+                  
+                  <!-- <table class="table table-borderless">
+                    <tbody>
+                      <tr>
+                        <td>Iced White Mocha</td>
+                        <td>$250.22</td>
+                        <td>1</td>
+                      </tr>
+                    </tbody>
+                  </table> -->
+                  <!-- <div class="col-lg-12 col-sm-12 col-12 cart-detail-product">
+                    <p>Iced White Mocha</p>
+                    <span class="price text-info"> $250.22</span> <span class="count"> Quantity:01</span>
+                  </div> -->
+                </div>
+                <div class="row total-header-section">
+                  <div class="col-lg-6 col-sm-6 col-6 total-section text-right">
+                    <hr>
+                    <p>Total: <span class="text-info">$<?=$sum?></span></p>
+                  </div>
+                </div>
+                <div class="row mt-4">
+                  <div class="col-lg-12 col-sm-12 col-12 text-center checkout">
+                    <button class="btn btn-primary btn-block">
+                      <a href="<?php echo base_url("/order_confirm") ?>" style="color:white; text-decoration:none">
+                        Checkout
+                      </a>
+                    </button>
+                  </div>
+                </div>
+              </div>
+          </li>
+          <li class=" nav-item <?= ($uri->getSegment(1) == 'user' ? 'active' : null) ?>">
+            <a class="nav-link px-2" href="<?php echo base_url('user'); ?>"><?php echo session()->get('username'); ?></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-2" href="<?php echo base_url("logout") ?>">Logout</a>
+          </li>
+          <script>
+            let isOpen = false;
+            let item = document.querySelector(".dropdown-menu");
 
-                function toggle() {
+            function toggle() {
 
-                    isOpen = !isOpen;
-                    item.style.display = isOpen ? "block" : "none";
-                    document.addEventListener("keydown", function(e) {
-                        if (e.key === "Escape") {
-                            isOpen = false;
-                            item.style.display = isOpen ? "block" : "none";
-                        }
-                    })
-                    document.addEventListener("click", function(e) {
-                        if (!item.contains(e.target) && !document.querySelector(".dropdown-toggle").contains(e
-                                .target)) {
-                            isOpen = false
-                            item.style.display = isOpen ? "block" : "none";
-                        }
-                    })
+              isOpen = !isOpen;
+              item.style.display = isOpen ? "block" : "none";
+              document.addEventListener("keydown", function(e) {
+                if (e.key === "Escape") {
+                  isOpen = false;
+                  item.style.display = isOpen ? "block" : "none";
                 }
-                </script>
-                <?php else : ?>
-                <li class="nav-item <?= ($uri->getSegment(1) == 'login' ? 'active' : null) ?>">
-                    <a class="nav-link px-2" href="<?php echo base_url('login'); ?>">Login</a>
-                </li>
-                <li class="nav-item <?= ($uri->getSegment(1) == 'register' ? 'active' : null) ?>">
-                    <a class="nav-link px-2" href="<?php echo base_url('register'); ?>">Signup</a>
-                </li>
-                <?php endif; ?>
-                <!-- <li class="nav-item">
+              })
+              document.addEventListener("click", function(e) {
+                if (!item.contains(e.target) && !document.querySelector(".dropdown-toggle").contains(e.target)) {
+                  isOpen = false
+                  item.style.display = isOpen ? "block" : "none";
+                }
+              })
+            }
+          </script>
+        <?php else : ?>
+          <li class="nav-item <?= ($uri->getSegment(1) == 'login' ? 'active' : null) ?>">
+            <a class="nav-link px-2" href="<?php echo base_url('login'); ?>">Login</a>
+          </li>
+          <li class="nav-item <?= ($uri->getSegment(1) == 'register' ? 'active' : null) ?>">
+            <a class="nav-link px-2" href="<?php echo base_url('register'); ?>">Signup</a>
+          </li>
+        <?php endif; ?>
+        <!-- <li class="nav-item">
                     cart</li> -->
-            </ul>
-        </nav>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light px-5 frontof">
-            <a class="navbar-brand" style="
-    font-family: 'Sansita Swashed', cursive;" href="<?php echo base_url(); ?>">Harold's Coffee</a>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav font-weight-normal">
-                    <li class="nav-item">
-                        <a class="nav-link text-dark" href="<?php echo base_url("/menu") ?>">Menu</a>
-                    </li>
-                    <li class="nav-item">
-                        <span class="nav-link px-0">|</span>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-dark" href="#">Store</a>
-                    </li>
-                    <li class="nav-item">
-                        <span class="nav-link px-0">|</span>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-dark" href="#">Help</a>
-                    </li>
-                </ul>
-            </div>
-    </div>
+      </ul>
     </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light px-5 frontof">
+      <a class="navbar-brand" style="font-family: 'Sansita Swashed', cursive;" href="<?php echo base_url(); ?>">Harold's
+        Coffee</a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav font-weight-normal">
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="<?php echo base_url("/menu") ?>">Menu</a>
+          </li>
+          <li class="nav-item">
+            <span class="nav-link px-0">|</span>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Store</a>
+          </li>
+          <li class="nav-item">
+            <span class="nav-link px-0">|</span>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Help</a>
+          </li>
+        </ul>
+      </div>
     </nav>
-    </div>
+
+  </div>
