70fc7ff4e99fbc45466ee16e931f7bcd59595760
ttphuongvi <trthphuongvigmail.com>
2022-04-01 13:48:55 +0700

    delivered history card

--- app/Views/pages/history.php
+++ app/Views/pages/history.php
@@ -46,7 +46,7 @@
 			</section>
         </div>   -->
 
-<nav style="width: 60%; margin: 0 auto">
+<nav style="width: 40%; margin: 0 auto">
   <div class="nav nav-tabs" id="nav-tab" role="tablist">
     <button class="nav-link active" id="nav-confirmed-tab" data-bs-toggle="tab" data-bs-target="#nav-confirmed" type="button" role="tab" aria-controls="nav-confirmed" aria-selected="true">Confirmed</button>
     <button class="nav-link" id="nav-delivering-tab" data-bs-toggle="tab" data-bs-target="#nav-delivering" type="button" role="tab" aria-controls="nav-delivering" aria-selected="false">Delivering</button>
@@ -56,37 +56,82 @@
 </nav>
 <div class="tab-content" id="nav-tabContent">
   <div class="tab-pane fade show active between" id="nav-confirmed" role="tabpanel" aria-labelledby="nav-confirmed-tab">Đã xác nhận</div>
+  
   <div class="tab-pane fade between" id="nav-delivering" role="tabpanel" aria-labelledby="nav-delivering-tab">Đang giao</div>
+
   <div class="tab-pane fade between" id="nav-delivered" role="tabpanel" aria-labelledby="nav-delivered-tab">
-      <div style="margin: 0 auto;">
-    <table class="table-history">
-            <tr class="form-title-info-history col-history">
-                <th>Name</th>
-                <th>Size</th>
-                <th>Amount</th>
-                <th>Price</th>
-            </tr>
-            <tr class="form-content-history" >
+      <!-- <div style="margin: 0 auto;">
+          <table class="table table-history">
+            <thead class="thead-light">
+              <tr>
+                <th scope="col">STT</th>
+                <th scope="col">Name</th>
+                <th scope="col">Size</th>
+                <th scope="col">Amount</th>
+                <th scope="col">Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
                 <td>Iced White Mocha</td>
                 <td>M</td>
                 <td>1</td>
                 <td>5.50</td>
-            </tr>
-            <tr class="form-content-history" >
-                <td>Iced White Mocha</td>
-                <td>M</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Pink Drink</td>
+                <td>S</td>
                 <td>1</td>
-                <td>5.50</td>
-            </tr>
+                <td>3.25</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Java Chip Frappuccino</td>
+                <td>L</td>
+                <td>2</td>
+                <td>6.25</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        </table>
-        <hr style="width: 60%; margin: 0 auto; border-top: 0.5px dotted"></hr>
+         <hr style="width: 60%; margin: 0 auto; border-top: 0.5px dotted"></hr>
         <div style="text-align: right; margin-right: 20%; margin-top: 15px">
             <div class="">Totals: <span>4.5</span> </div>
             <button class="repurchase-history right-history">Repurchase</button>
-        </div>
+        </div> -->
+        <div class="card-history" >
+          <div class="flex-row-history space-history">
+            <div class="flex-row-history ">
+              <div>
+                <img src="<?php echo base_url("/img/coffee.jpg") ?>"  alt="..." class="img-coffee"></img>
+              </div>
+              <div class="flex-col-history margin-1">
+                <div class="flex-row-history space-history">
+                  <div>ID order: </div>
+                  <div>52487VVHFI45</div>
+                </div>
+                <div class="flex-row-history">
+                  <div>Quantity: </div>
+                  <div>2</div>
+                </div>
+                <div class="flex-row-history">
+                  <div>Size: </div>
+                  <div>L, M, S</div>
+                </div>
+              </div>
+            </div>
+            <div class="flex-col-history">
+              <button class="btn-view-order-detail">View order details</button>
+              <button>Order again</button>
+            </div>
+          </div>
+      </div>
     </div>
     </div>
+
+
   <div class="tab-pane fade between" id="nav-cancelled" role="tabpanel" aria-labelledby="nav-cancelled-tab">Đã hủy</div>
 </div>
 
