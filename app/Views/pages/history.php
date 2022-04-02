fc76a859d4f0295feee7ebdef62c6bd55929dca2
ttphuongvi <trthphuongvigmail.com>
2022-04-02 12:47:33 +0700

    complited task

--- app/Views/pages/history.php
+++ app/Views/pages/history.php
@@ -7,49 +7,119 @@
   </div>
 </nav>
 <div class="tab-content" id="nav-tabContent">
-  <div class="tab-pane fade show active between" id="nav-confirmed" role="tabpanel" aria-labelledby="nav-confirmed-tab">Đã xác nhận</div>
-
-  <div class="tab-pane fade between" id="nav-delivering" role="tabpanel" aria-labelledby="nav-delivering-tab">Đang giao</div>
-
-  <div class="tab-pane fade between" id="nav-delivered" role="tabpanel" aria-labelledby="nav-delivered-tab">
-        <div class="card-history" >
-          <div style=" color: #756e6e; display: flex; margin-right: 5px; margin-bottom: 10px"> Delivered on 
-            <div style="margin-left: 5px;">27/03/2022</div>
-        </div>
-          <div class="flex-row-history space-history">
-            <div class="flex-row-history ">
-              <div>
-                <img src="<?php echo base_url("/img/coffee.jpg") ?>"  alt="..." class="img-coffee"></img>
-              </div>
-              <div class="flex-col-history margin-1">
-                <div class="flex-row-history space-history">
-                  <div class="bold-history margin-2">ID order: </div>
-                  <div>#52487VVHFI45</div>
-                </div>
-                <div class="flex-row-history">
-                  <div class="bold-history margin-2">Quantity: </div>
-                  <div>2</div>
-                </div>
-                <div class="flex-row-history">
-                  <div class="bold-history margin-2">Size: </div>
-                  <div>L, M, S</div>
-                </div>
-                <div class="flex-row-history">
-                  <div class="bold-history margin-2">Totals: </div>
-                  <div>4.5</div>
-                </div>
-              </div>
+  <div class="tab-pane fade show active between" id="nav-confirmed" role="tabpanel" aria-labelledby="nav-confirmed-tab">
+    <div class="card-history">
+      <div class="date-order"> Confirmed on
+        <div style="margin-left: 5px;">27/03/2022</div>
+      </div>
+      <div class="flex-row-history space-history">
+        <div class="flex-row-history ">
+          <div>
+            <img src="<?php echo base_url("/img/coffee.jpg") ?>" alt="..." class="img-coffee"></img>
+          </div>
+          <div class="flex-col-history margin-1">
+            <div class="flex-row-history space-history">
+              <div class="bold-history margin-2">ID order: </div>
+              <div>#52487VVHFI45</div>
             </div>
-            <div class="flex-col-history">
-              <a class="btn-view-order-detail" href="<?php echo base_url("/order_details.php") ?>">View order details</a>
-              <a class="btn-order-again" href="<?php echo base_url("") ?>">Order again</a>
+            <div class="flex-row-history">
+              <div class="bold-history margin-2">Quantity: </div>
+              <div>2</div>
+            </div>
+            <div class="flex-row-history">
+              <div class="bold-history margin-2">Size: </div>
+              <div>L, M, S</div>
+            </div>
+            <div class="flex-row-history">
+              <div class="bold-history margin-2">Totals: </div>
+              <div>4.5</div>
             </div>
           </div>
+        </div>
+        <div class="flex-col-history">
+          <a class="btn-view-order-detail" href="<?php echo base_url("/order_details.php") ?>">View order details</a>
+          <a class="btn-order-again" href="<?php echo base_url("") ?>">Order again</a>
+        </div>
       </div>
     </div>
-    </div>
+  </div>
 
+
+
+
+  <div class="tab-pane fade between" id="nav-delivering" role="tabpanel" aria-labelledby="nav-delivering-tab">
+    <div class="card-history">
+      <div class="date-order"> Delivering on
+        <div style="margin-left: 5px;">27/03/2022</div>
+      </div>
+      <div class="flex-row-history space-history">
+        <div class="flex-row-history ">
+          <div>
+            <img src="<?php echo base_url("/img/coffee.jpg") ?>" alt="..." class="img-coffee"></img>
+          </div>
+          <div class="flex-col-history margin-1">
+            <div class="flex-row-history space-history">
+              <div class="bold-history margin-2">ID order: </div>
+              <div>#52487VVHFI45</div>
+            </div>
+            <div class="flex-row-history">
+              <div class="bold-history margin-2">Quantity: </div>
+              <div>2</div>
+            </div>
+            <div class="flex-row-history">
+              <div class="bold-history margin-2">Size: </div>
+              <div>L, M, S</div>
+            </div>
+            <div class="flex-row-history">
+              <div class="bold-history margin-2">Totals: </div>
+              <div>4.5</div>
+            </div>
+          </div>
+        </div>
+        <div class="flex-col-history">
+          <a class="btn-view-order-detail" href="<?php echo base_url("/order_details.php") ?>">View order details</a>
+          <a class="btn-order-again" href="<?php echo base_url("") ?>">Order again</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="tab-pane fade between" id="nav-delivered" role="tabpanel" aria-labelledby="nav-delivered-tab">
+    <div class="card-history">
+      <div class="date-order"> Delivered on
+        <div style="margin-left: 5px;">27/03/2022</div>
+      </div>
+      <div class="flex-row-history space-history">
+        <div class="flex-row-history ">
+          <div>
+            <img src="<?php echo base_url("/img/coffee.jpg") ?>" alt="..." class="img-coffee"></img>
+          </div>
+          <div class="flex-col-history margin-1">
+            <div class="flex-row-history space-history">
+              <div class="bold-history margin-2">ID order: </div>
+              <div>#52487VVHFI45</div>
+            </div>
+            <div class="flex-row-history">
+              <div class="bold-history margin-2">Quantity: </div>
+              <div>2</div>
+            </div>
+            <div class="flex-row-history">
+              <div class="bold-history margin-2">Size: </div>
+              <div>L, M, S</div>
+            </div>
+            <div class="flex-row-history">
+              <div class="bold-history margin-2">Totals: </div>
+              <div>4.5</div>
+            </div>
+          </div>
+        </div>
+        <div class="flex-col-history">
+          <a class="btn-view-order-detail" href="<?php echo base_url("/order_details.php") ?>">View order details</a>
+          <a class="btn-order-again" href="<?php echo base_url("") ?>">Order again</a>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <div class="tab-pane fade between" id="nav-cancelled" role="tabpanel" aria-labelledby="nav-cancelled-tab">Đã hủy</div>
 </div>
-
