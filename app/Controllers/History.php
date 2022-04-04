8e3356a083073cc64a51975f513e063dadc545de
HuynhManDat <JuniorKevin110@gmail.com>
2022-04-04 11:42:27 +0700

    Update cart in controller after merge branch

--- app/Controllers/History.php
+++ app/Controllers/History.php
@@ -3,14 +3,18 @@
 namespace App\Controllers;
 
 use App\Models\OrderDelivery;
+use App\Models\CartModel;
 
 class History extends BaseController
 {
     public function index()
     {
         $model = new OrderDelivery();
+        $cart_model = new CartModel();
+        $user_id = session()->has('id') ? session()->get('id') : NULL;
         $data["list_orders"] = $model->getAllOrder();
-        echo view("templates/header");
+        $data["cart_item"] = $cart_model->getUserCart($user_id);
+        echo view("templates/header", $data);
         echo view("pages/history", $data);
         echo view("templates/footer");
     }
