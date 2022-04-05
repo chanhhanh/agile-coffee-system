ffaab5a62487b104265ce2c5fc1a153484632026
ttphuongvi <trthphuongvigmail.com>
2022-04-05 15:16:09 +0700

    add method cancel order

--- app/Controllers/History.php
+++ app/Controllers/History.php
@@ -7,15 +7,16 @@ use App\Models\CartModel;
 
 class History extends BaseController
 {
+
     public function index()
     {
-        $model = new OrderDelivery();
+        $order_delivery_model = new OrderDelivery();
         $cart_model = new CartModel();
 
         $user_id = session()->has('id') ? session()->get('id') : NULL;
 
         $data_history = [
-            'list_orders' => $model->getAllOrder($user_id),
+            'list_orders' => $order_delivery_model->getAllOrder($user_id),
         ];
 
         $data = [
@@ -27,5 +28,18 @@ class History extends BaseController
         echo view("templates/header", $data);
         echo view("pages/history", $data_history);
         echo view("templates/footer");
+    }
+
+    public function cancelOrder()
+    {
+        $request = \Config\Services::request();
+        $user_id = session()->has('id') ? session()->get('id') : NULL;
+        $order_id = $request->getPost('order_id');
+
+        // TODO valid order id of user
+        $order_delivery_model = new OrderDelivery();
+        $order_delivery_model->update($order_id, ['status' => 4]);
+
+        return redirect()->route('history');
     }
 }
