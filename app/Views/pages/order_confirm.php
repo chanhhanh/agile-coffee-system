9bb21f7af71e87a2377fdba0672c4b14eb85244c
ttphuongvi <trthphuongvigmail.com>
2022-04-14 14:14:02 +0700

    change style button

--- app/Views/pages/order_confirm.php
+++ app/Views/pages/order_confirm.php
@@ -34,31 +34,28 @@
                         <ul class="list-group mb-3" style="width:451px">
                             <?php $sum = 0 ?>
                             <?php foreach ($cart_item as $item) : ?>
-                            <li class="product list-group-item d-flex justify-content-between lh-condensed">
-                                <div style="width: 262px">
-                                    <h6 class="my-0"><?php echo $item["coffee_name"] ?> (<?php echo $item["size"] ?>)
-                                    </h6>
-                                    <small class="text-muted product-line-price">Customized:
-                                        Size: <?php echo $item["size"] ?>,
-                                        Sweetness: <?php echo $item["sweetness"] ?>,
-                                        Milk: <?php echo $item["milk"] ?></small>
-                                </div>
+                                <li class="product list-group-item d-flex justify-content-between lh-condensed">
+                                    <div style="width: 262px">
+                                        <h6 class="my-0"><?php echo $item["coffee_name"] ?> (<?php echo $item["size"] ?>)
+                                        </h6>
+                                        <small class="text-muted product-line-price">Customized:
+                                            Size: <?php echo $item["size"] ?>,
+                                            Sweetness: <?php echo $item["sweetness"] ?>,
+                                            Milk: <?php echo $item["milk"] ?></small>
+                                    </div>
 
-                                <div class="product-quantity" style="width: 52px; margin-left: 10px">
-                                    <input type="number" value="<?php echo $item["quantity"] ?>" min="1"
-                                        style="width:40px">
-                                </div>
-                                <div class="product-removal" style="margin-right: -18px; width: 95px">
-                                    <!-- <button class="remove-product"
+                                    <div class="product-quantity" style="width: 52px; margin-left: 10px">
+                                        <input type="number" value="<?php echo $item["quantity"] ?>" min="1" style="width:40px">
+                                    </div>
+                                    <div class="product-removal" style="margin-right: -18px; width: 95px">
+                                        <!-- <button class="remove-product"
                                             style="border: 0; padding: 4px 8px; background-color: #c66; color: #fff; font-family: font-bold; font-size: 12px; border-radius: 3px;">
                                             Remove
                                         </button> -->
-                                    <i class="bi bi-trash3 remove-product"
-                                        style="border: 0; padding: 4px 8px; background-color: #c66; color: #fff; font-family: font-bold; font-size: 12px; border-radius: 3px;"></i>
-                                    <span class="text-muted"
-                                        style="padding-left:15px"><?php echo $item["total_amount"] ?></span>
-                            </li>
-                            <?php $sum += $item["total_amount"] ?>
+                                        <i class="bi bi-trash3 remove-product" style="border: 0; padding: 4px 8px; background-color: #c66; color: #fff; font-family: font-bold; font-size: 12px; border-radius: 3px;"></i>
+                                        <span class="text-muted" style="padding-left:15px"><?php echo $item["total_amount"] ?></span>
+                                </li>
+                                <?php $sum += $item["total_amount"] ?>
                             <?php endforeach; ?>
                             <!-- <li class="list-group-item d-flex justify-content-between lh-condensed">
                                 <div>
@@ -79,17 +76,14 @@
                             </div>
                         </div> -->
                         <hr class="mb-4">
-                        <button class="btn btn-primary btn-lg btn-block" type="submit" name="btnDatHang"
-                            style="margin-top: 0; margin-bottom: 0; margin-left: 353px; margin-right: 0;">Order</button>
+                        <button class="btn btn-secondary btn-lg btn-block" type="submit" name="btnDatHang" style="margin-top: 0; margin-bottom: 0; margin-left: 353px; margin-right: 0;">Order</button>
                     </div>
                     <div class="col-md-8 order-md-1" style="width: 600px; margin-left:30px">
                         <h4 class="mb-3">Customer information</h4>
                         <div class="row">
                             <div class="col-md-12">
                                 <label for="exampleInputName">Full Name</label>
-                                <input type="text" class="form-control" id="exampleInputName"
-                                    aria-describedby="nameHelp" placeholder="Your full name"
-                                    value="<?php echo session()->get('fullname'); ?>">
+                                <input type="text" class="form-control" id="exampleInputName" aria-describedby="nameHelp" placeholder="Your full name" value="<?php echo session()->get('fullname'); ?>">
                             </div>
                             <!-- <div class="col-md-12">
                                 <label for="exampleInputAddress">Address</label>
@@ -98,30 +92,22 @@
                             </div> -->
                             <div class="col-md-12">
                                 <label for="exampleInputPhone">Phone</label>
-                                <input id="exampleInputPhone" name="areaNo" class="form-control" type="tel" required
-                                    aria-describedby="phoneHelp" placeholder="Your phone" pattern="[0-9]{10}"
-                                    title="Please Enter Correct Phone Format" aria-label="Area code"
-                                    value="<?php echo session()->get('phone_number'); ?>">
+                                <input id="exampleInputPhone" name="areaNo" class="form-control" type="tel" required aria-describedby="phoneHelp" placeholder="Your phone" pattern="[0-9]{10}" title="Please Enter Correct Phone Format" aria-label="Area code" value="<?php echo session()->get('phone_number'); ?>">
                             </div>
                             <div class="col-md-12">
                                 <label for="exampleInputEmail">Email</label>
                                 <!-- <input type="text" class="form-control" id="exampleInputEmail"
                                     aria-describedby="emailHelp" placeholder="Your email"> -->
-                                <input class="form-control" id="exampleInputEmail" type="email" size="64" maxLength="64"
-                                    required placeholder="username@gmail.com" pattern=".+@gmail\.com"
-                                    title="Please Enter Correct Email Format"
-                                    value="<?php echo session()->get('email'); ?>">
+                                <input class="form-control" id="exampleInputEmail" type="email" size="64" maxLength="64" required placeholder="username@gmail.com" pattern=".+@gmail\.com" title="Please Enter Correct Email Format" value="<?php echo session()->get('email'); ?>">
                             </div>
                             <h4 class="mb-3">Method order</h4>
                             <div class="d-block my-3">
                                 <div class="custom-control custom-radio">
-                                    <input id="httt-1" name="httt_ma" type="radio" class="custom-control-input"
-                                        required="" value="1">
+                                    <input id="httt-1" name="httt_ma" type="radio" class="custom-control-input" required="" value="1">
                                     <label class="custom-control-label" for="httt-1">Delivery</label>
                                 </div>
                                 <div class="custom-control custom-radio">
-                                    <input id="httt-2" name="httt_ma" type="radio" class="custom-control-input"
-                                        required="" value="2">
+                                    <input id="httt-2" name="httt_ma" type="radio" class="custom-control-input" required="" value="2">
                                     <label class="custom-control-label" for="httt-2">Pickup</label>
                                 </div>
                             </div>
@@ -130,22 +116,18 @@
                         <!--  Tiền mặt MoMo ZaloPay ShopeePay-->
                         <div class="d-block my-3">
                             <div class="custom-control custom-radio">
-                                <input id="httt-1" name="httt_ma" type="radio" class="custom-control-input" required=""
-                                    value="1">
+                                <input id="httt-1" name="httt_ma" type="radio" class="custom-control-input" required="" value="1">
                                 <label class="custom-control-label" for="httt-1">Cash on Delivery</label>
                             </div>
                             <div class="custom-control custom-radio">
-                                <input id="httt-2" name="httt_ma" type="radio" class="custom-control-input" required=""
-                                    value="2">
+                                <input id="httt-2" name="httt_ma" type="radio" class="custom-control-input" required="" value="2">
                                 <label class="custom-control-label" for="httt-2">Direct Bank
                                     Transfer</label>
                             </div>
                             <div class="custom-control custom-radio">
-                                <input id="httt-3" name="httt_ma" type="radio" class="custom-control-input" required=""
-                                    value="3">
+                                <input id="httt-3" name="httt_ma" type="radio" class="custom-control-input" required="" value="3">
                                 <label class="custom-control-label" for="httt-3">Paypal</label>
-                                <img src="https://www.logolynx.com/images/logolynx/c3/c36093ca9fb6c250f74d319550acac4d.jpeg"
-                                    alt="" width="50">
+                                <img src="https://www.logolynx.com/images/logolynx/c3/c36093ca9fb6c250f74d319550acac4d.jpeg" alt="" width="50">
                             </div>
                         </div>
                     </div>
